dad036c584f312ae1087eb5261a99d9233e6cb34
Prathap <prathap.p@safe.security>
2022-08-21 23:30:07 +0530

    Add initial version of safe logger

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import winston from "winston";
+declare const logger: winston.Logger;
+export default logger;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import winston from "winston";
+
+const { combine, errors, timestamp, splat, json } = winston.format;
+
+const logger = winston.createLogger({
+
+    // default log level is "info"
+    level: "info",
+
+    // combining multiple formats to get the desired output
+    format: combine(
+
+        // required to log errors thrown by the application; ignored otherwise
+        errors({ stack: true }),
+
+        // enables string interpolation of messages
+        splat(),
+
+        // adds timestamp to all log messages
+        timestamp(),
+
+        // default log format is JSON
+        json()
+    ),
+
+    transports: [
+        
+        // logs will be written to console
+        new winston.transports.Console({
+
+            // catch and log `uncaughtException` events from the application
+            handleExceptions: true,
+
+            // catch and log `uncaughtRejection` events from the application
+            handleRejections: true
+        })
+    ],
+
+    // generic metadata applied to all logs
+    defaultMeta: { type: "application" }
+});
+
+export default logger;
