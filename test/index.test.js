712fa894acf82334e4ab115862ad38d8664d02ca
prathap-safe <prathap.p@safe.security>
2022-08-30 12:00:04 +0530

    Add test files to validate the functionality

---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+const logger = require("logger-safe-security").default;
+const appLogger = logger.child({ service: "sample" });
+
+logger.info("This is a parent logger");
+appLogger.info("This is a child logger");
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+import logger from "logger-safe-security";
+const appLogger = logger.child({ service: "sample" });
+
+logger.info("This is a parent logger");
+appLogger.info("This is a child logger");
