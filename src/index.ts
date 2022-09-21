651d15ec436c4b6d4d63914aa1b1ad1881bad47f
Aman Jain (ECS/DEL) <aman.j@safe.security>
2022-09-21 12:45:03 +0530

    fix: style issues

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,40 @@ import winston from "winston";
 const { combine, errors, timestamp, splat, json } = winston.format;
 
 export const createLoggerInstance = ({
-  logLevel = "info",
+    logLevel = "info"
 }: {
-  logLevel: string;
-}) =>
-  winston.createLogger({
-    // default log level is "info"
-    level: logLevel,
+    logLevel: string;
+}): winston.Logger =>
+    winston.createLogger({
+        // default log level is "info"
+        level: logLevel,
 
-    // combining multiple formats to get the desired output
-    format: combine(
-      // required to log errors thrown by the application; ignored otherwise
-      errors({ stack: true }),
+        // combining multiple formats to get the desired output
+        format: combine(
+            // required to log errors thrown by the application; ignored otherwise
+            errors({ stack: true }),
 
-      // enables string interpolation of messages
-      splat(),
+            // enables string interpolation of messages
+            splat(),
 
-      // adds timestamp to all log messages
-      timestamp(),
+            // adds timestamp to all log messages
+            timestamp(),
 
-      // default log format is JSON
-      json()
-    ),
+            // default log format is JSON
+            json()
+        ),
 
-    transports: [
-      // logs will be written to console
-      new winston.transports.Console({
-        // catch and log `uncaughtException` events from the application
-        handleExceptions: true,
+        transports: [
+            // logs will be written to console
+            new winston.transports.Console({
+                // catch and log `uncaughtException` events from the application
+                handleExceptions: true,
 
-        // catch and log `uncaughtRejection` events from the application
-        handleRejections: true,
-      }),
-    ],
+                // catch and log `uncaughtRejection` events from the application
+                handleRejections: true
+            })
+        ],
 
-    // generic metadata applied to all logs
-    defaultMeta: { type: "application" },
-  });
+        // generic metadata applied to all logs
+        defaultMeta: { type: "application" }
+    });
