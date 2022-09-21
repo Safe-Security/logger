7cf7daa3a6cd95a890971b0779e41b7c52f92f3e
Aman Jain (ECS/DEL) <aman.j@safe.security>
2022-09-21 12:26:46 +0530

    feat: add support for loglevels

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,37 @@ import winston from "winston";
 
 const { combine, errors, timestamp, splat, json } = winston.format;
 
-export const logger = winston.createLogger({
-
+export const logger = ({ logLevel = "info" }: { logLevel: string }) =>
+  winston.createLogger({
     // default log level is "info"
-    level: "info",
+    level: logLevel,
 
     // combining multiple formats to get the desired output
     format: combine(
+      // required to log errors thrown by the application; ignored otherwise
+      errors({ stack: true }),
 
-        // required to log errors thrown by the application; ignored otherwise
-        errors({ stack: true }),
+      // enables string interpolation of messages
+      splat(),
 
-        // enables string interpolation of messages
-        splat(),
+      // adds timestamp to all log messages
+      timestamp(),
 
-        // adds timestamp to all log messages
-        timestamp(),
-
-        // default log format is JSON
-        json()
+      // default log format is JSON
+      json()
     ),
 
     transports: [
-        
-        // logs will be written to console
-        new winston.transports.Console({
+      // logs will be written to console
+      new winston.transports.Console({
+        // catch and log `uncaughtException` events from the application
+        handleExceptions: true,
 
-            // catch and log `uncaughtException` events from the application
-            handleExceptions: true,
-
-            // catch and log `uncaughtRejection` events from the application
-            handleRejections: true
-        })
+        // catch and log `uncaughtRejection` events from the application
+        handleRejections: true,
+      }),
     ],
 
     // generic metadata applied to all logs
-    defaultMeta: { type: "application" }
-});
+    defaultMeta: { type: "application" },
+  });
