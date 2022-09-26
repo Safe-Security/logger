4505b08be6a5768334e06de7096d4f2b29677fc4
Aman Jain (ECS/DEL) <aman.j@safe.security>
2022-09-26 19:05:46 +0530

    feat: updated logger to user metadata

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,39 @@
 import winston from "winston";
 
-const { combine, errors, timestamp, splat, json } = winston.format;
+const { combine, errors, timestamp, splat, json, metadata } = winston.format;
 
 export const levels = winston.config.npm.levels;
 
 export type Logger = winston.Logger;
 
-const formatMessageMetadata = winston.format(
-    ({ message, level, service, type, ...rest }) => {
-        return {
-            message,
-            level,
-            type,
-            ...(service && { service }),
-            messageMetadata: rest,
-            // this is required as per the winston documentation: https://github.com/winstonjs/winston#streams-objectmode-and-info-objects
-            [Symbol.for("level")]: level,
-            [Symbol.for("message")]: message
-        };
-    }
-);
-
 export const createLogger = (
     {
-        logLevel = "info",
+        level = "info",
         service
     }: {
-        logLevel?: string;
+        level?: string;
         service?: string;
-    } = { logLevel: "info" }
+    } = { level: "info" }
 ): winston.Logger =>
     winston.createLogger({
         // default log level is "info"
-        level: logLevel,
+        level,
 
         // combining multiple formats to get the desired output
         format: combine(
-            // moves all the other fields in the the message to `messageMetadata` property
-            formatMessageMetadata(),
-            
             // required to log errors thrown by the application; ignored otherwise
             errors({ stack: true }),
+
+            // adds timestamp to all log messages
+            timestamp(),
 
             // enables string interpolation of messages
             splat(),
 
-            // adds timestamp to all log messages
-            timestamp(),
+            // moves all the other fields in the message to `metadata` property
+            metadata({
+                fillExcept: ["message", "level", "timestamp", "service", "type"]
+            }),
 
             // default log format is JSON
             json()
@@ -69,4 +56,3 @@ export const createLogger = (
         // generic metadata applied to all logs
         defaultMeta: { type: "application", ...(service && { service }) }
     });
-
