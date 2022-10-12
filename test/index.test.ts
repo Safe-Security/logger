3061b4f2b7d18a03c56e76d20b35ef74ba8ef6c5
Aman Jain <61691832+amanjain14@users.noreply.github.com>
2022-10-12 20:41:24 +0530

    fix: updated metadata to exclude error object (#16)
    Co-authored-by: Aman Jain (ECS/DEL) <aman.j@safe.security>

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,3 +3,8 @@ const logger = createLogger({ service: "sample" });
 
 logger.info("This is a sample logger");
 logger.info("This is a sample logger with more information", { planet: "Earth" });
+
+
+/* Logging the error property. */
+const err = new Error("This is a sample Error");
+logger.info("Error occurred while performing the operation", { error: err });
