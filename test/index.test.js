dd0dc7f2cf7a52ca1beaa0bed9197bf7c9b371d7
Roopak Chugh <roopak.c@safe.security>
2024-11-21 23:10:56 +0530

    updated tests

--- test/index.test.js
+++ test/index.test.js
@@ -9,3 +9,40 @@ logger.info("This is a sample logger with more information", {
 /* Logging the error property. */
 const err = new Error("This is a sample Error");
 logger.info("Error occurred while performing the operation", { error: err });
+
+const metadataWithSensitiveData = {
+  planet: "Earth",
+  userEmail: "test@test.com",
+  userName: "John Doe",
+  userRole: "admin",
+  userId: 1234567890,
+  nesting: {
+    userEmail: "test@test.com",
+    userName: "John Doe",
+    userRole: "admin",
+    userId: 1234567890,
+    stringified: JSON.stringify({
+      userEmail: "test@test.com",
+      userName: "John Doe",
+      userRole: "admin",
+      userId: 1234567890,
+    }),
+  },
+};
+
+// mask the metadata
+logger.info(
+  "This is a sample logger with masked metadata",
+  metadataWithSensitiveData
+);
+
+const loggerWithCustomMaskFields = createLogger({
+  config: {
+    maskFields: ["userEmail"],
+  },
+});
+
+loggerWithCustomMaskFields.info(
+  "This is a sample logger with masked metadata",
+  metadataWithSensitiveData
+);
